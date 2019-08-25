fbe4b8826298de9b2a976a1fbd8b055e3a9826f4
India Hiring <indiahiring@global.publicisgroupe.net>
2019-08-25 14:26:50 +0530

    Add sample test

---
+++ assignment/src/test/java/sapient/app/assignment/controller/StandingsControllerTest.java
@@ -0,0 +1,12 @@
+package sapient.app.assignment.controller;
+
+
+import org.junit.Test;
+
+public class StandingsControllerTest {
+
+    @Test
+    public void test() {
+        assert(true);
+    }
+}
